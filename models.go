6a380ebcfb82fca458ce7ce1c60dff8cfd3febac
hoysoft <hoysoft@qq.com>
2014-11-13 21:05:30 +0800

    Update models.go

--- models.go
+++ models.go
@@ -1,297 +1,191 @@
 package authlogin
 
 import (
-	//"fmt"
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/session"
-	//	"time"
-	"html/template"
-	//"log"
-	//"strconv"
-	"bytes"
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	"io"
+	"reflect"
+	"strings"
+	"time"
 )
 
-type MethodFunc func(*beego.Controller, string, string, bool)
-
-var (
-	globalSessions *session.Manager
-
-	loginHtmlString  string
-	mLayout, LoginT  *template.Template
-	actionMethodFunc MethodFunc
-)
-
-type UserController struct {
-	beego.Controller
+// 用户表
+type User struct {
+	Id            int    `orm:"auto;PK"` // 用户ID
+	Email         string `orm:"size(32)` // 用户Email
+	Username      string `orm:"size(32)` // 用户名
+	Password      string `orm:"size(32)` // 密码；加密形式
+	Nickname      string `orm:"size(32)"`
+	Remark        string `orm:"size(200);null"`
+	Status        int
+	Createdtime   time.Time `orm:"auto_now_add;type(datetime)"`               // 用户创建时间
+	Updatedtime   time.Time `orm:"auto_now;type(datetime)"`                   // 用户最后修改时间
+	Lastlogintime time.Time `orm:"column(lastlogintime);type(datetime);null"` // 用户最后登录时间
 }
 
 func init() {
-	globalSessions, _ = session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "booksmanage", "cookieLifeTime": 3600, "providerConfig": ""}`)
-	go globalSessions.GC()
-
-	//var err error
-
-	mLayout, _ = template.New("layout").Parse(`<!DOCTYPE html>
-<html>
-<head><title>{{.Status}} Not Authorized</title></head>
-<body>
- {{.LayoutContent}}
-</body>
-</html>`)
-
-	LoginT, _ = template.New("LayoutContent").Parse(`
-<div class="login">
-<h1>{{.Status}} Not Authorized - {{.Message}}</h1>
-<form action="{{.Url}}" method="post">
-<label for="username">username: </label>
-<input type="text" name="username" id="username" value="" placeholder="(Enter your username)"><br>
-<label for="password">Password: </label>
-<input type="password" name="password" id="password" value="" placeholder="(Enter your password)"><br>
-<input type='checkbox' name='save_login' value='1' checked='checked'/> 记住我的登录信息<br>
-<br>
-<a href="/user/reset-pwd">忘记登录密码？</a> 
-<input type='button' value='注册'/> 
-<input type="submit" value="登录">
-
-</form>
-</div>
-`)
-
-	//增加路由
-	beego.Router("/user", &UserController{})
-	beego.Router("/user/:action", &UserController{})
+	orm.RegisterModel(new(User))
 
 }
 
-func ActionMethod(methodFunc MethodFunc) {
-	actionMethodFunc = methodFunc
-}
+//空表时增加默认管理帐号
+func autoAddTable() {
+	o := orm.NewOrm()
+	count, _ := o.QueryTable("user").Count()
 
-// 检测用户是否登录
-func LoginUser(this *beego.Controller, autoRedirect bool) *User {
-	sess := globalSessions.SessionStart(this.Ctx.ResponseWriter, this.Ctx.Request)
-	defer sess.SessionRelease(this.Ctx.ResponseWriter)
-	userid := sess.Get("Uid")
-	if userid != nil {
-		user, e := GetUserById(userid.(int))
-		if e == nil {
-			return user //用户已经登录，返回用户信息
+	if count == 0 {
+		u := User{Id: 0, Username: "admin", Password: Sha1("888888")}
+		_, er := AddUser(&u)
+		if er != nil {
+			fmt.Println("add user error:%s", er)
 		}
 	}
-
-	if this.Ctx.Request.RequestURI != "/user/login" && autoRedirect { // 用户未登录，自动跳转
-		this.Ctx.Redirect(302, "/user/login") // 跳转到用户登录页面
-	}
-	return nil
 }
 
-func (this *UserController) Get() {
-	//this.Layout = tpLayout                    // 后台管理模板布局文件
-	action := this.Ctx.Input.Param(":action") // 用户的添加、修改或删除
+//验证用户
+func AuthUser(name string, password string) (*User, bool) {
+	var user User
+	o := orm.NewOrm()
+	err := o.QueryTable("user").Filter("Username", name).Filter("password", Sha1(password)).One(&user)
 
-	switch action {
-	case "": //用户列表
-		users := GetAllUse_sm()
-		this.Data["Users"] = users
+	if err != nil || &user == nil {
+		//增加默认管理帐号
+		autoAddTable()
+	}
+	if &user != nil {
+		// 更新用户最后登录时间信息.
+		user.Lastlogintime = time.Now()
+		UpdateUserById(&user)
+	}
+	return &user, err == nil
+}
 
-	case "login": // 用户登录
-		data := make(map[string]interface{})
-		data["Status"] = "YYYY"
-		data["Url"] = "/user/login"
+//增加用户
+func AddUser(m *User) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
 
-		buff := bytes.NewBufferString("")
-		LoginT.Execute(buff, data)
+//修改用户
+func UpdateUserById(m *User) (err error) {
+	o := orm.NewOrm()
+	v := User{Id: m.Id}
+	if err := o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
 
-		if actionMethodFunc != nil {
-			defaultContent := true
-			actionMethodFunc(&this.Controller, "Get", "login", defaultContent)
-			if !defaultContent {
-				return
+//删除用户
+func DeleteUser(id int) (err error) {
+	o := orm.NewOrm()
+	v := User{Id: id}
+	if err := o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&User{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
+
+func GetUserById(id int) (v *User, err error) {
+	o := orm.NewOrm()
+	v = &User{Id: id}
+	if err := o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+func GetAllUse_sm() *[]User {
+	var l []User
+	o := orm.NewOrm()
+	o.QueryTable(new(User)).All(&l)
+	return &l
+}
+
+func GetAllUser(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(User))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		qs = qs.Filter(k, v)
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
 			}
-
-			this.HtmlContent = buff.String()
-			return
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
 		}
-
-		mLayout.Execute(this.Ctx.ResponseWriter, data)
-
-		//this.Ctx.WriteString(loginT)
-
-		//case "logout": // 用户退出
-		//	this.Layout = "layout_one.tpl"     // 用户登录模板布局文件
-		//	this.DelSession("account")         // 删除session中的用户登录信息
-		//	this.TplNames = "admin/logout.tpl" // 页面模板文件
-	case "add": //注册用户
-	//this.Ctx.WriteString(loginT)
-	case "reset-pwd": //密码复位
-	case "logout": // 用户退出
-		sess := globalSessions.SessionStart(this.Ctx.ResponseWriter, this.Ctx.Request)
-		defer sess.SessionRelease(this.Ctx.ResponseWriter)
-		sess.Delete("Uid")
-		this.Ctx.Redirect(302, "/")
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
 	}
 
+	var l []User
+	qs = qs.OrderBy(sortFields...)
+	if _, err := qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
 }
 
-func (this *UserController) Post() {
-	//	this.Layout = "layout_admin.tpl"           // 模板布局文件
-	action := this.Ctx.Input.Param(":action") // 用户的添加或修改
-	switch action {
-	//case "add": // 添加用户
-	//	email := this.Input().Get("email")            // 用户E-mail
-	//	name := this.Input().Get("name")              // 用户名
-	//	password := this.Input().Get("password")      // 密码
-	//	rePassword := this.Input().Get("re-password") // 重复输入的密码
-
-	//	// 检测E-mail或密码是否为空
-	//	if email == "" || name == "" {
-	//		this.Data["Message"] = "E-mail或用户名为空"
-	//		this.Data["Email"] = email
-	//		this.Data["Name"] = name
-	//		this.Data["Password"] = password
-	//		this.TplNames = "admin/add_user.tpl"
-	//		return
-	//	}
-
-	//	// 如果两次输入的密码不一致，需重新填写
-	//	if password != rePassword {
-	//		this.Data["Message"] = "两次输入的密码不一致"
-	//		this.Data["Email"] = email
-	//		this.Data["Name"] = name
-	//		this.Data["Password"] = password
-	//		this.TplNames = "admin/add_user.tpl"
-	//		return
-	//	}
-
-	//	// 检查E-mail或用户名是否已存在
-	//	orm = InitDb()
-	//	user := User{}
-	//	err = orm.Where("email=? or name=?", email, name).Find(&user)
-	//	if err == nil {
-	//		this.Data["Message"] = "E-mail或用户名已存在"
-	//		this.Data["Email"] = email
-	//		this.Data["Name"] = name
-	//		this.Data["Password"] = password
-	//		this.TplNames = "admin/add_user.tpl"
-	//		return
-	//	}
-
-	//	// 保存用户
-	//	orm = InitDb()
-	//	user = User{}
-	//	user.Email = email
-	//	user.Name = name
-	//	user.Password = Sha1(password)
-	//	user.Created = time.Now()
-	//	err = orm.Save(&user)
-	//	Check(err)
-	//	Debug("User `%s` added.", user)
-
-	//	this.Ctx.Redirect(302, "/user/") // 返回用户列表页面
-	//case "edit": // 修改用户
-	//	id := this.Ctx.Input.Params(":id") // 用户ID
-
-	//	email := this.Input().Get("email")            // 用户E-mail
-	//	name := this.Input().Get("name")              // 用户名
-	//	password := this.Input().Get("password")      // 密码
-	//	rePassword := this.Input().Get("re-password") // 重复输入的密码
-
-	//	// 检测E-mail或密码是否为空
-	//	if email == "" || name == "" {
-	//		this.Data["Message"] = "E-mail或用户名为空"
-	//		this.Data["Email"] = email
-	//		this.Data["Name"] = name
-	//		this.Data["Password"] = password
-	//		this.TplNames = "admin/add_user.tpl"
-	//		return
-	//	}
-
-	//	// 如果两次输入的密码不一致，需重新填写
-	//	if password != rePassword {
-	//		this.Data["Message"] = "两次输入的密码不一致"
-	//		this.Data["Email"] = email
-	//		this.Data["Name"] = name
-	//		this.Data["Password"] = password
-	//		this.TplNames = "admin/add_user.tpl"
-	//		return
-	//	}
-
-	//	// 获得当前用户
-	//	orm = InitDb()
-	//	user := User{}
-	//	err = orm.Where("id=?", id).Find(&user)
-	//	Check(err)
-
-	//	// 更新用户信息
-	//	user.Email = email
-	//	user.Name = name
-	//	if password != "" {
-	//		user.Password = Sha1(password)
-	//	}
-	//	user.Updated = time.Now()
-
-	//	// 保存用户信息
-	//	err = orm.Save(&user)
-	//	Check(err)
-
-	//	this.Ctx.Redirect(302, "/user/") // 返回用户列表页面
-	case "login": // 用户登录
-		name := this.Ctx.Request.FormValue("username")     // 用户名
-		password := this.Ctx.Request.FormValue("password") // 用户密码
-		// 检测用户名或密码是否为空
-		if name == "" || password == "" {
-			this.Data["Message"] = "用户名或密码为空"
-			this.Ctx.Redirect(302, "/user/login")
-			return
-		}
-		user, b := AuthUser(name, password)
-		if !b || user == nil {
-			this.Data["Message"] = "用户名或密码错误！"
-			this.Ctx.Redirect(302, "/user/login")
-			return
-		} else {
-			this.Data["Message"] = "登录成功！"
-			// 保存用户登录信息
-			sess := globalSessions.SessionStart(this.Ctx.ResponseWriter, this.Ctx.Request)
-			defer sess.SessionRelease(this.Ctx.ResponseWriter)
-			sess.Set("Uid", user.Id)
-			this.Ctx.Redirect(302, "/v1/site")
-		}
-		// 如果不存在任何用户，那么直接以admin身份登录
-		//orm = InitDb()
-		//users := []User{}
-		//err = orm.FindAll(&users)
-		//if err == nil && len(users) == 0 {
-		//	this.SetSession("Uid", "0")
-		//	this.Ctx.Redirect(302, "/admin/")
-		//}
-
-		//	errFlag := false // 判断登录是否出错
-
-		//	orm = InitDb()
-		//	user := User{}
-		//	err = orm.Where("name=? and password=?", name, Sha1(password)).Find(&user)
-		//	if err != nil {
-		//		this.Data["Message"] = "用户名或密码错误"
-		//		errFlag = true
-		//	} else { // 用户名、密码验证成功
-		//		// 保存用户登录信息
-		//		sess.Set("account", name)
-
-		//		this.Ctx.Redirect(302, "/admin/") // 跳转到管理后台首页
-		//	}
-
-		//	// 显示用户登录页面，再次登录
-		//	if errFlag {
-		//		this.Data["Name"] = name
-		//		this.Data["Password"] = password
-		//		this.Layout = "layout_one.tpl"
-		//		SiteName := beego.AppConfig.String("appname") // 网站名称
-		//		this.Data["SiteName"] = SiteName
-		//		this.Data["Categories"] = GetCategories() // 分类列表，用于导航栏
-		//		this.TplNames = "admin/login.tpl"
-		//		return
-		//	}
-
-	}
+//对字符串进行SHA1哈希
+func Sha1(data string) string {
+	t := sha1.New()
+	io.WriteString(t, data)
+	return fmt.Sprintf("%x", t.Sum(nil))
 }
